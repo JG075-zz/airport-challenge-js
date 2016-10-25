4f67d6c5dc4ee463c0cfe5f53667c8e8b502f8bc
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-25 13:48:35 +0100

    Land a plane

---
+++ jasmine-standalone-2.5.2/spec/airportSpec.js
@@ -0,0 +1,18 @@
+describe("Airport", function() {
+  var airport;
+  var plane;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = new Plane();
+  });
+
+  it("should be able to land a plane", function() {
+    airport.land(plane);
+    expect(airport.planes).toContain(plane);
+  });
+
+
+
+
+});
